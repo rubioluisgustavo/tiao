af5143a0da4115298b788e6369a2274dc48ae865
Luis Rubio <rubioluisgustavo@outlook.com>
2022-05-12 15:02:35 -0300

    estilização

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
 import axios from 'axios';
 
 function App() {
   const [albums, setAlbums] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
-  const token = 'rubioluisgustavo@outlook.com';
+  const token = 'rubioluisgustav@outlook.com';
   const url = 'https://tiao.supliu.com.br/api/album';
 
   const configs = {
@@ -16,7 +16,22 @@ function App() {
     }
   };
 
-  const deleteAlbum = ((id) => {
+  const timeConvert = (duration) => {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+    const fill = ('00' + minutes).slice(2);
+    const time = hours + ":" + fill;
+    return time;
+  };
+
+  const createTrack = ((id) => {
+    axios.post(url, configs, { album_id: 1, number: 123, title: "Track nova", duration: 130 }).then((response) => {
+    }).catch((error) => {
+      alert(error);
+    })
+  })
+
+  const deleteTrack = ((id) => {
     axios.delete(`${url}/${id}`, configs).then((response) => {
       alert(response.data);
     })
@@ -24,41 +39,61 @@ function App() {
 
   const createAlbum = ((id) => {
     axios.post(url, configs, { name: "As melhores", year: "1949" }).then((response) => {
-      console.log(response);
+      alert(response);
     }).catch((error) => {
-      console.log(error);
+      alert(error);
     })
   })
 
-  const searchAlbum = ((id) => {
-    axios.get(url, configs).then((response) => {
-      setAlbums(response.data.data);
-    }).catch((error) => {
-      console.log(error);
+  const deleteAlbum = ((id) => {
+    axios.delete(`${url}/${id}`, configs).then((response) => {
+      alert(response.data);
     })
   })
 
   useEffect(() => {
     axios.get(url, configs).then((response) => {
       setAlbums(response.data.data);
+      setTracks(response.data.data[0].tracks);
     })
   }, [])
 
   return (
     <div className="App">
-      {albums.map((item) => {
-        return (
-          <div key={item.id}>
-            <p>
-              {item.name}
-            </p>
-            <span>
-              {item.year}
-            </span>
-            <button type='button' onClick={() => deleteAlbum(item.id)}>Excluir</button>
-          </div>
-        );
-      })}
+
+      <h1 className='title'>Tião Carreiro & Pardinho</h1>
+
+      <main className="container">
+        <h2 className="title">Álbuns</h2>
+        <div className="albums-grid">
+          {albums.map((item) => {
+            return (
+              <div key={item.id} className="album-card">
+                <p className="album-title">{item.name}</p>
+                <span className="album-year">{item.year}</span>
+              </div>
+            );
+          })}
+        </div>
+
+        <h2 className="title">Faixas</h2>
+        <div className="tracks">
+          {tracks.map((track) => {
+            return (<div key={track.id} className="track-wrapper">
+              <div className="track-details">
+                <span className="track-number">{track.number}</span>
+                <div className="track">
+                  <p className="name">{track.title}</p>
+                </div>
+              </div>
+              <div className="track-play">
+                <span className="track-time">{timeConvert(track.duration)}</span>
+              </div>
+            </div>);
+          })}
+        </div>
+        <button onClick={createTrack()} className='btn btn-success'>Cadastrar Faixa</button>
+      </main>
     </div>
   );
 }
