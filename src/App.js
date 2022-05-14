9fd1141795da73064596f25399cf7ac42f6cda51
Luis Rubio <rubioluisgustavo@outlook.com>
2022-05-14 13:58:39 -0300

    ajustes finais e validações de campos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios";
+import logo from '../src/logo.png'
 
 export const App = () => {
   // ALBUMS STATES
   const [albums, setAlbums] = useState([]);
   const [nameAlbum, setNameAlbum] = useState("");
   const [yearAlbum, setYearAlbum] = useState("");
-
-  // TRACKS STATES
   const [nameTrack, setNameTrack] = useState("");
   const [numberTrack, setNumberTrack] = useState("");
   const [durationTrack, setDurationTrack] = useState("");
-
-  //CONFIGS 
   const token = "rubioluisgustavo@gmail.com";
   const url = "https://tiao.supliu.com.br/api/album";
   const urlTrack = "https://tiao.supliu.com.br/api/track";
-  const configs = {
-    headers: {
-      "Content-type": "application/json",
-      Authorization: token,
-    },
-  };
+  const configs = { headers: { "Content-type": "application/json", Authorization: token, } };
 
   const searchAlbum = (value) => {
     axios
@@ -33,11 +25,28 @@ export const App = () => {
       .then((response) => {
         setAlbums(response.data.data);
       });
-    console.log(value);
   };
 
   const createAlbum = () => {
+    var time = new Date();
+    var currentyear = time.getFullYear();
+
     const data = { name: nameAlbum, year: yearAlbum };
+
+    if (data.name === "") {
+      alert("Digite o nome do álbum!")
+      return false;
+    }
+
+    if (data.year.length < 4) {
+      alert("O ano deve ter 4 dígitos!");
+      return false;
+    }
+
+    if (data.year > currentyear) {
+      alert("O ano não pode ser maior que o ano atual!");
+      return false;
+    }
 
     axios
       .post(url, data, configs)
@@ -56,6 +65,18 @@ export const App = () => {
       number: numberTrack,
       duration: durationTrack,
     };
+    if (data.title === "") {
+      alert("Digite o nome da faixa!");
+      return false;
+    }
+    if (data.number === "") {
+      alert("Digite o número da faixa!");
+      return false;
+    }
+    if (data.duration === "") {
+      alert("Digite a duração da faixa!");
+      return false;
+    }
     axios
       .post(urlTrack, data, configs)
       .then((response) => {
@@ -101,38 +122,40 @@ export const App = () => {
   return (
     <div className="App">
       <main className="container">
-        <h1 className="title">Tião Carreiro e Pardinho - Discografia</h1>
-        <div>
-          <h2 className="title">Buscar Álbum</h2>
+      <img src={logo} alt='Logo da aplicação' />
+        discografia<h1 className="title">TIÃO CARREIRO E PARDINHO</h1>
+        <div className="buscadastro">
+          <h2 className="title align-text-left">Olá, usuário! Qual álbum você quer escutar hoje?</h2>
           <input type="text" placeholder="Nome do Álbum" onChange={(e) => searchAlbum(e.target.value)} />
           <div>
             <h2 className="title">Cadastrar Álbum</h2>
             <input onChange={(e) => setNameAlbum(e.target.value)} placeholder="Nome do Álbum" />
             <input onChange={(e) => setYearAlbum(e.target.value)} style={{ marginLeft: 20 }} placeholder="Ano (4 dígitos)" />
-            <button type="button" className="create" onClick={() => { createAlbum(); }}>Criar</button>
+            <button type="button" className="create" onClick={() => { createAlbum(); }}>CRIAR</button>
           </div>
         </div>
 
-        <h2 className="title">Álbuns</h2>
+        <h2 className="title">Álbuns Cadastrados</h2>
         <div className="albums-grid">
           {albums.map((item) => {
             return (
-              
               <div key={item.id} className="album-card">
                 <div className="nome-album">
                   <div className="d-flex flex-column">
                     <p className="album-title">{item.name}</p>
                     <span className="album-year">{item.year}</span>
                   </div>
-                  <button type="button" className="remove" onClick={() => deleteAlbum(item.id)}>Excluir Álbum</button>
+                  <button type="button" className="remove" onClick={() => deleteAlbum(item.id)}>EXCLUIR</button>
                 </div>
 
                 <div>
-                  <h2 className="title">Cadastrar Faixa</h2>
+                  <h2 className="title">Faixas</h2>
                   <input onChange={(e) => setNameTrack(e.target.value)} placeholder="Nome da faixa" />
                   <input onChange={(e) => setNumberTrack(e.target.value)} placeholder="Número da faixa" />
-                  <input onChange={(e) => setDurationTrack(e.target.value)} placeholder="Duração da faixa" />
-                  <button type="button" className="create" onClick={() => { createTrack(item.id); }}>Criar</button>
+                  <input onChange={(e) => setDurationTrack(e.target.value)} placeholder="Duração da faixa (segundos)" />
+                  <div className="criarfaixa">
+                    <button type="button" className="create" onClick={() => { createTrack(item.id); }}>CRIAR</button>
+                  </div>
                 </div>
 
                 <div>
@@ -152,7 +175,7 @@ export const App = () => {
                               <span className="title">{track.title}</span>
                             </div>
                             <div><span className="duration">{track.duration}</span>
-                              <button type="button" className="remove" onClick={() => deleteTrack(track.id)}>Excluir Faixa</button>
+                              <button type="button" className="remove" onClick={() => deleteTrack(track.id)}>EXCLUIR</button>
                             </div>
                           </li>
                         </>
